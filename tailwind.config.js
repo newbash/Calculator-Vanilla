f5c5daef6f228769e097e0cb73804c6b66f2c533
newbash <newbashiq@outlook.com>
2024-11-15 21:08:05 -0600

    Made the Base/Whole App UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     './main.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        warmWhite: {
+          DEFAULT: '#F8F4F0', // Base background
+          light: '#FAF8F5',    // Lighter accent
+          dark: '#F2E8DC',
+        },
+        cozyBrown: {
+          light: '#A58D7F',    // Subtle text/accents
+          DEFAULT: '#7A5E47',  // Body text
+          dark: '#5A4635',     // Headlines/strong contrast
+        },
+        shadowBrown: {
+          soft: '#CBB49A',     // Soft shadow
+          medium: '#9C7A5A',   // Moderate shadow
+          deep: '#4A3325',     // Deep shadow
+        },
+      }
+    },
     darkMode: 'class',
   },
   plugins: [],
